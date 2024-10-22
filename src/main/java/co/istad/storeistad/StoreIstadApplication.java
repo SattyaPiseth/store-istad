985b747de66dba7e75021d1e04a5daaac78e93a6
piseth sattya <pisethsattya33@gmail.com>
2024-10-23 01:19:54 +0700

    testing

--- src/main/java/co/istad/storeistad/StoreIstadApplication.java
+++ src/main/java/co/istad/storeistad/StoreIstadApplication.java
@@ -1,7 +1,5 @@
 package co.istad.storeistad;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreIstadApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(StoreIstadApplication.class, args);
-        for (int i = 0; i < 1000; i++) {
-            System.out.println("b sl o  jumauah jumauah");
-        }
+        SpringApplication.run(StoreIstadApplication.class, args);
     }
 
 }
